5207bea348b1f4f5216a8c23cf3f60d145ad559e
StephenMak <stephen.makrogianni@silverstripe.com>
2019-08-15 14:05:44 +1200

    PHP 7.3 compatible

--- _config.php
+++ _config.php
@@ -1,8 +1,6 @@
 <?php
 
 //object extensions
-Object::add_extension('BlogTree','BlogCategoryTree');
-Object::add_extension('BlogTree_Controller','BlogCategoryTreeExtension_Controller');
-Object::add_extension('BlogEntry','BlogCategoryEntry');
-
-?>
+SS_Object::add_extension('BlogTree','BlogCategoryTree');
+SS_Object::add_extension('BlogTree_Controller','BlogCategoryTreeExtension_Controller');
+SS_Object::add_extension('BlogEntry','BlogCategoryEntry');
